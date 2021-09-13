8ab245dd3ba09ee4d0f4c5b55260220ecf20f4a5
Blest-1 <81591931+Blest-1@users.noreply.github.com>
2021-09-13 12:25:28 -0600

    Add files via upload

---
+++ Rpms.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Method.Override
+{
+    public class Rpms:Engines
+    {
+        public override void Engine()
+        {
+            base.Engine();
+            Console.WriteLine("Dino Testing RPMs.");
+        }
+
+    }
+}
